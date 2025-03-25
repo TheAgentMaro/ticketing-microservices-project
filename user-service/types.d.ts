569f9b00d069e926fc697267ec325d326af54476
Med Marwen Meddeb <marwen.meddeb1812@gmail.com>
2025-03-25 18:07:45 +0100

    Mise à jour du Dockerfile pour inclure tous les fichiers TypeScript et les répertoires nécessaires. Ajout du fichier types.d.ts pour les déclarations de modules.

---
+++ user-service/types.d.ts
@@ -0,0 +1,5 @@
+// Déclarations de modules pour les bibliothèques sans fichiers de types
+declare module 'swagger-ui-express';
+declare module 'yamljs';
+declare module 'jsonwebtoken';
+declare module 'bcrypt';
